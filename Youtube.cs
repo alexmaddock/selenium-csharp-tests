cabf287f030e470b338792f92c5ed50c7cdfe48a
Alex Maddock <alexmaddock@Alexs-MBP-2.fritz.box>
2023-10-03 11:56:08 +1000

    add working sample too loop through video selections and select appropriate title

--- Youtube.cs
+++ Youtube.cs
@@ -8,7 +8,7 @@ using NUnit;
 namespace Selenium {
 
     [TestFixture] 
-    public class SeleniumTest : BrowserState
+    public class YoutubeTest : BrowserState
     {
 
         [Test]
@@ -19,19 +19,21 @@ namespace Selenium {
 
             var searchBox = driver.FindElement(By.XPath("//input[@id='search']"));
             searchBox.Click();
-            searchBox.SendKeys("9 Top Automated Testing Practices to Follow.");
+            searchBox.SendKeys(" 9 Top Automated Testing Practices to Follow. ");
             var searchIcon = driver.FindElement(By.Id("search-icon-legacy"));
             searchIcon.Click();
 
             Thread.Sleep(5000);
             var titles = driver.FindElements(By.Id("video-title"));//.GetAttribute("innerText");
+
+            // IList<IWebElement> titlesList = titles; // not needed
+
             foreach(IWebElement title in titles) {
-                // Console.WriteLine(title.Text);
-                if(title.Text.Contains("9 Top Automated Testing Practices to Follow."))
-                    Console.WriteLine("Found title, selecting" + " " + title.Text);
-                    // Console.WriteLine("ELEMENT IS: ", title);
+
+                if(title.Text.Contains("9 Top Automated Testing Practices")) {
+                    Console.WriteLine("Found matching text" + " " + title.Text + "Clicking it....");
                     title.Click();
-    
+                }
             }
             
             // Console.WriteLine(titleFind);
